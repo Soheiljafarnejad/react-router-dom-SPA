758d58f6fba66b2579fa1706bee6ff34dd587294
soheiljafarnejad <jafarnejad.official@gmail.com>
2022-03-25 21:34:38 +0430

    not found

--- src/Pages/NotFound.js
+++ src/Pages/NotFound.js
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
+
 const NotFound = (props) => {
-  console.log(props);
   return (
     <div>
       <h2>error 404</h2>
       <h3>Not found page !</h3>
+      <Link to="/">go to home</Link>
     </div>
   );
 };
